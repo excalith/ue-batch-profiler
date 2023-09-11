7bc80b0d8c00bed1bce0856b32232fac25cd3519
Can Cellek <can.cellek@gmail.com>
2023-09-11 14:40:58 +0300

    Update plugin build dependencies

--- Source/BatchProfiler/BatchProfiler.Build.cs
+++ Source/BatchProfiler/BatchProfiler.Build.cs
@@ -8,60 +8,25 @@ public class BatchProfiler : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 		
-		PrivateIncludePaths.AddRange(
-			new [] 
-			{
-				System.IO.Path.GetFullPath(Target.RelativeEnginePath) + "/Source/Editor/EditorStyle/Public"
-			}
-		);
-				
-		
-		PrivateIncludePaths.AddRange(
-			new string[] {
-				// ... add other private include paths required here ...
-			}
-			);
-			
-		
 		PublicDependencyModuleNames.AddRange(
 			new []
 			{
-				"Core",
-				"Blutility",
-				"EditorScriptingUtilities",
-				"UMG",
-				"Niagara",
-				"UnrealEd",
-				"AssetRegistry",
-				"AssetTools",
-				"ContentBrowser",
-				"InputCore",
-				"Slate",
-				"SlateCore",
-				"Projects"
-				// ... add other public dependencies that you statically link with here ...
-			}
-		);
-			
-		
-		PrivateDependencyModuleNames.AddRange(
-			new []
-			{
-				"CoreUObject",
-				"Engine",
-				"Slate",
-				"SlateCore", 
-				"EditorStyle"
-				// ... add private dependencies that you statically link with here ...	
+				"Core"
+				, "CoreUObject"
+				, "Engine"
+				, "InputCore"
 			}
 		);
 		
-		
-		DynamicallyLoadedModuleNames.AddRange(
-			new string[]
+		if (Target.bBuildEditor == true)
+		{
+			PublicDependencyModuleNames.AddRange(new string[]
 			{
-				// ... add any modules that your module loads dynamically here ...
-			}
-			);
+				"Slate"
+				, "SlateCore"
+				, "UnrealEd"
+				, "MainFrame"
+			});
+		}
 	}
 }
